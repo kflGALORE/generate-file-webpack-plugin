86855a462fb47280be0698c9b55a3e9155741553
kflGALORE <kflgalore@gmail.com>
2020-03-25 19:24:15 +0100

    next micro version

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import * as webpack from "webpack";
 
 export class GenerateFileWebpackPlugin {
-    public constructor(public options: any) {
+    public constructor(public options: Options) {
     }
 
     public apply(compiler: webpack.Compiler):void {
         compiler.hooks.emit.tap('GenerateFileWebpackPlugin', (compilation, callback) => {
-            console.log('Hello World');
+            compilation.getLogger('GenerateFileWebpackPlugin').info()
+            console.log('Hello World:' + this.options.file);
         });
     }
+}
+
+interface Options {
+    file: string;
 }
