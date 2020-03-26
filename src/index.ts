e7a98ac425aa987e95abfbb30745a3038f29244c
kflGALORE <kflgalore@gmail.com>
2020-03-26 22:29:59 +0100

    implements test case generate-static-content

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as webpack from "webpack";
+import * as fs from 'fs';
+import * as path from 'path';
 
 export class GenerateFileWebpackPlugin {
     public constructor(public options: Options) {
@@ -6,12 +8,22 @@ export class GenerateFileWebpackPlugin {
 
     public apply(compiler: webpack.Compiler):void {
         compiler.hooks.emit.tap('GenerateFileWebpackPlugin', (compilation, callback) => {
-            compilation.getLogger('GenerateFileWebpackPlugin').info()
-            console.log('Hello World:' + this.options.file);
+            if (typeof this.options.content === 'string' ||  this.options.content instanceof String) {
+                const dir = path.dirname(this.options.file);
+                if (! fs.existsSync(dir)) {
+                    fs.mkdirSync(dir, {recursive: true})
+                }
+                console.log('generating file ' + this.options.file + ' with content ' + this.options.content);
+                fs.writeFileSync(this.options.file,  this.options.content);
+            } else {
+                // what?
+            }
+            //compilation.getLogger('GenerateFileWebpackPlugin').info()
         });
     }
 }
 
 interface Options {
     file: string;
+    content: any;
 }
---
+++ test/webpack.spec.ts
@@ -0,0 +1,69 @@
+//import webpack from 'webpack';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as rimraf from 'rimraf';
+import * as webpack from "webpack";
+import DoneCallback = jest.DoneCallback;
+
+describe('webpack', () => {
+    it('should generate static content', (done) => {
+        test(done, 'generate-static-content');
+    });
+});
+
+function test(done: DoneCallback, testCase: string) {
+    const testDir = path.resolve(__dirname, 'webpack/' + testCase);
+    const expectedOutputDir = path.resolve(testDir, 'expected');
+
+    const outputDir = emptyDir(path.resolve(__dirname, '.tmp/webpack/' + testCase));
+    process.env.outputDir = path.resolve(testDir,outputDir);
+
+    const configFile = path.resolve(testDir, 'webpack.config.js');
+    const config = require(configFile);
+
+    // Run Webpack
+    webpack(config, (webpackError, webpackStats) => {
+        if (webpackError) {
+            return done(webpackError);
+        }
+        if (webpackStats.hasErrors()) {
+            return done(webpackStats.toString());
+        }
+        try {
+            fs.readdirSync(expectedOutputDir).forEach((file) => {
+                const expectedFile = path.resolve(expectedOutputDir, file);
+                const actualFile = path.join(outputDir, file);
+
+                try {
+                    expect(fs.existsSync(actualFile)).toBe(true);
+                } catch (e) {
+                    throw new Error('file-exists [' + actualFile + ']\n' + e.toString());
+                }
+                try {
+                    expect(fileContent(actualFile)).toEqual(fileContent(expectedFile));
+                } catch (e) {
+                    throw new Error('file-content-equals [' + actualFile + '] [' + expectedFile + ']\n' + e.toString());
+                }
+            });
+            done();
+        } catch (e) {
+            done(e);
+        }
+    });
+}
+
+function fileContent(path: string): string | null {
+    if (! fs.existsSync(path)) {
+        return null;
+    }
+    return fs.readFileSync(path, 'utf-8');
+}
+
+function emptyDir(path: string): string {
+    if (fs.existsSync(path)) {
+        rimraf.sync(path);
+    }
+    fs.mkdirSync(path, {recursive: true});
+
+    return path;
+}
